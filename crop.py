ac58cc15731f1f54744b7ef9d3e48f8374a8c850
Atilla Yasar <112716905+AtillaYasar@users.noreply.github.com>
2023-03-27 16:13:17 +0200

    Update crop.py

--- crop.py
+++ crop.py
@@ -1,9 +1,10 @@
 from pydub import AudioSegment
 
-f_name = 'elven ranger'
+f_name = 'something something'
 f_type = 'm4a'
 path = f'{f_name}.{f_type}'
 track = AudioSegment.from_file(f'{f_name}.{f_type}', format=f_type)
 
+# will cut it from 3:00 to 10:00.  (180 sec, 600 sec)
 track[180*1000:600*1000].export(f'{f_name}_out.{f_type}', format=f_type)
 
