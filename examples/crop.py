41685be300ce060025ba5f48be6933c0ae3e90e1
Atilla Yasar <112716905+AtillaYasar@users.noreply.github.com>
2023-03-27 16:11:13 +0200

    Delete examples directory

--- examples/crop.py
+++
@@ -1,9 +0,0 @@
-from pydub import AudioSegment
-
-f_name = 'elven ranger'
-f_type = 'm4a'
-path = f'{f_name}.{f_type}'
-track = AudioSegment.from_file(f'{f_name}.{f_type}', format=f_type)
-
-track[180*1000:600*1000].export(f'{f_name}_out.{f_type}', format=f_type)
-
